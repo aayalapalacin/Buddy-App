c8aad921d3cee8f0fd0abdf4bb97794304f777d5
willdodsonn <sodlax38@gmail.com>
2022-04-01 23:52:49 +0000

    login functionality complete

--- src/front/js/component/LoginPage/LoginApp.jsx
+++ src/front/js/component/LoginPage/LoginApp.jsx
@@ -1,28 +1,40 @@
 import React, {useState} from "react";
-import LoginForm from "./LoginForm";
+import LoginForm from "./LoginForm.jsx";
 import "./login.css";
+
+
 const LoginApp = () => {
   const adminUser = {
-    admin: "admin@admin.com",
-    password: "admin123",
+    username: "David Dodson",
+    password: "admin",
   };
 
-  const [user, setUser] = useState({ email: "" });
+  const [user, setUser] = useState({ username: ""});
   const [error, setError] = useState("");
 
   const Login = (details) => {
     console.log(details);
+
+    if(details.username == adminUser.username && details.password == adminUser.password){
+      console.log("You did it")
+      setUser({
+        username: details.username
+      });
+    } else {
+      console.log("Doesn't match")
+      setError("Doesn't match")
+    }
   };
   const Logout = () => {
-    console.log("Logout");
+    setUser({username: ""})
   };
 
   return (
     <div className="login-app">
-        {(user.email != "") ? (
+        {(user.username != "") ? (
             <div className="welcome">
-                <h2>Welcome,<span>{user.name}</span></h2>
-                <button>Logout</button>
+                <h2>Welcome,<span>{user.username}</span><img src="https://humornama.com/wp-content/uploads/2022/03/No-Bitches-Meme-Template-on-Megamind-1-768x1024.jpg" height={300}/></h2>
+                <button onClick={Logout}>Logout</button>
             </div>
         ) : (
       <div className="login-container">
---
+++ src/front/js/component/LoginPage/LoginForm.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import "./login.css";
+const LoginForm = ({ Login, error }) => {
+  const [details, setDetails] = useState({ username: "", password: "" });
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    Login(details);
+  };
+  return (
+    <>
+      <div className="inputs">
+        <form onSubmit={submitHandler}>
+          {(error != "") ? (<div className="error">{error}</div>) : ""}
+          <label>
+            <input
+              type="text"
+              className="username"
+              name="name"
+              placeholder="Username"
+              id="username"
+              onChange={e => setDetails({...details, username: e.target.value})} 
+              value={details.username}
+              />
+          </label>
+          <br></br>
+          <label>
+            <input
+              type="password"
+              className="password"
+              name="name"
+              placeholder="Password"
+              id="password"
+              onChange={e => setDetails({...details, password: e.target.value})} 
+              value={details.password}
+              />
+          </label>
+          <br></br>
+              
+          <a href="/">Forgot Password?</a>
+          <button type="submit" value="Login" className="login-button">Login</button>
+          <button className="account-button">Create a new account</button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default LoginForm;
