c6464df3df1c47e46f7e3f7585a5ebb8454224b4
avokeys <alexander.ayala.palacin@gmail.com>
2022-04-01 19:13:24 +0000

    no duplicate functionality on dayButton

--- src/front/js/component/dayBtn.jsx
+++ src/front/js/component/dayBtn.jsx
@@ -3,41 +3,6 @@ import "../../styles/home.css";
 import { useDrop } from "react-dnd";
 import Task from "./task.jsx";
 
-// import React, { useState } from 'react'
-// import { useDrop } from 'react-dnd';
-// import { PetCard } from './PetCard';
-
-// const PETS = [
-//     { id: 1, name: 'dog' },
-//     { id: 2, name: 'cat' },
-//     { id: 3, name: 'fish' },
-//     { id: 4, name: 'hamster' },
-// ]
-
-// export const Basket = () => {
-//     const [basket, setBasket] = useState([])
-//     const [{ isOver }, dropRef] = useDrop({
-//         accept: 'pet',
-//         drop: (item) => setBasket((basket) =>
-//                             !basket.includes(item) ? [...basket, item] : basket),
-//         collect: (monitor) => ({
-//             isOver: monitor.isOver()
-//         })
-//     })
-
-//     return (
-//         <React.Fragment>
-//             <div className='pets'>
-//                 {PETS.map(pet => <PetCard draggable id={pet.id} name={pet.name} />)}
-//             </div>
-//             <div className='basket' ref={dropRef}>
-//                 {basket.map(pet => <PetCard id={pet.id} name={pet.name} />)}
-//                 {isOver && <div>Drop Here!</div>}
-//             </div>
-//         </React.Fragment>
-//     )
-// }
-
 const TASKS = [
   { id: 1, name: "school work" },
   { id: 2, name: "Gym" },
@@ -49,10 +14,21 @@ const DayBtn = () => {
   const [dayButton, setDayButton] = useState([]);
   const [{ isOver }, dropRef] = useDrop({
     accept: "task",
-    drop: (item) =>
-      setDayButton((dayButton) =>
-        !dayButton.includes(item) ? [...dayButton, item] : dayButton
-      ),
+    drop: (item) => {
+      if (dayButton.length == 0) {
+        setDayButton([item]);
+      } else if (dayButton.length > 0) {
+        let list = dayButton.filter((taskItem) => {
+          if (taskItem.name != item.name) {
+            return taskItem;
+          }
+        });
+        list = [...list, item];
+        setDayButton(list);
+      }
+    },
+    // !dayButton.includes(item) ? [...dayButton, item] : dayButton
+
     collect: (monitor) => ({
       isOver: monitor.isOver(),
     }),
--- src/front/js/component/task.jsx
+++ src/front/js/component/task.jsx
@@ -2,22 +2,6 @@ import React from "react";
 import "../../styles/home.css";
 import { useDrag } from "react-dnd";
 
-// export const PetCard = ({ id, name }) => {
-//   const [{ isDragging }, dragRef] = useDrag({
-//     type: "pet",
-//     item: { id, name },
-//     collect: (monitor) => ({
-//       isDragging: monitor.isDragging(),
-//     }),
-//   });
-//   return (
-//     <div className="pet-card" ref={dragRef}>
-//       {name}
-//       {isDragging && "😱"}
-//     </div>
-//   );
-// };
-
 const Task = ({ id, name }) => {
   const [{ isDragging }, dragRef] = useDrag({
     type: "task",
