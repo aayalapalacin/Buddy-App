63198d37468cc0c3e61634a4fce37cfaee82d821
avokeys <alexander.ayala.palacin@gmail.com>
2022-04-05 01:17:08 +0000

    added dropdown to task buttons

--- src/front/js/component/task.jsx
+++ src/front/js/component/task.jsx
@@ -5,7 +5,15 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
 
 const Task = ({ id, name }) => {
-  const 
+  const [taskItem, setTaskItem] = useState("");
+  const [newTaskItem, setNewTaskItem] = useState([]);
+
+  const addTask = (event) => {
+    if (event) {
+      const addedTaskItem = [...newTaskItem, taskitem];
+      setNewTaskItem(addedTaskItem);
+    }
+  };
   const [{ isDragging }, dragRef] = useDrag({
     type: "task",
     item: { id, name },
@@ -14,18 +22,47 @@ const Task = ({ id, name }) => {
     }),
   });
   return (
-    <div>
+    <div className="btn-group">
       <button
         type="button"
-        className="btn btn-outline-info taskBtn"
+        className="btn btn-outline-info taskBtn dropdown-toggle"
         ref={dragRef}
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
       >
         {name}
         {isDragging && ""}
       </button>
+      <ul class="dropdown-menu">
+        <li>
+          <a class="dropdown-item" href="#">
+            Action
+          </a>
+        </li>
+        <li>
+          <a class="dropdown-item" href="#">
+            Separated link
+          </a>
+        </li>
+      </ul>
+
       <FontAwesomeIcon icon={faCirclePlus}></FontAwesomeIcon>
     </div>
   );
 };
 
+{
+  /* <div class="btn-group">
+  <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+    Action
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+    <li><hr class="dropdown-divider"></li>
+    <li><a class="dropdown-item" href="#">Separated link</a></li>
+  </ul>
+</div> */
+}
 export default Task;
