69009797420d26b8378e63577ff09765d914dc40
avokeys <alexander.ayala.palacin@gmail.com>
2022-04-05 00:22:24 +0000

    added plus circle button, working on useState for adding new items

--- src/front/js/component/task.jsx
+++ src/front/js/component/task.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/home.css";
 import { useDrag } from "react-dnd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
 
 const Task = ({ id, name }) => {
+  const 
   const [{ isDragging }, dragRef] = useDrag({
     type: "task",
     item: { id, name },
@@ -20,6 +23,7 @@ const Task = ({ id, name }) => {
         {name}
         {isDragging && ""}
       </button>
+      <FontAwesomeIcon icon={faCirclePlus}></FontAwesomeIcon>
     </div>
   );
 };
